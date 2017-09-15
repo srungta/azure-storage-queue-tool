d73cfab0e3f82fda18768dc9c168cf5e67512a99
Shaswat Rungta <shaswatrungta@hotmail.com>
2017-09-15 13:14:04 +0530

    Added examples

--- azure-storage-queue-tool/Program.cs
+++ azure-storage-queue-tool/Program.cs
@@ -1,9 +1,31 @@
-﻿namespace azure_storage_queue_tool
+﻿using System.Collections.Generic;
+
+namespace azure_storage_queue_tool
 {
     class Program
     {
+        private const string connectionString = "<Replace Connection String here and then delete later>";
         static void Main(string[] args)
         {
+            var queueName = "<Insert queue name here";
+            var queueClient = new QueueHelper(connectionString);
+
+            //Simple message example
+            queueClient.SendMessageToQueueAsync(queueName, "<Insert you message object here>", true).GetAwaiter().GetResult();
+
+            //Multiple message example
+            var sampleMultipleMessages = new List<SampleClass>
+            {
+                new SampleClass(){MyProperty1 = 1, MyProperty2 = "1"},
+                new SampleClass(){MyProperty1 = 2, MyProperty2 = "2"}
+            };
+            queueClient.SendBatchMessagesToQueueAsync(queueName, sampleMultipleMessages, true).GetAwaiter().GetResult();
         }
+    }
+
+    public class SampleClass
+    {
+        public int MyProperty1 { get; set; }
+        public string MyProperty2 { get; set; }
     }
 }
